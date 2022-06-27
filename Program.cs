87a8cc95af8c8f2578d1a118095bcda9fec72a65
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-06-27 14:20:38 +0200

    snack 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int sum = 0;
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("Inserisci un numero ");
+    int newNum = Int32.Parse(Console.ReadLine());
+    sum += newNum;
+};
+Console.WriteLine(sum);
