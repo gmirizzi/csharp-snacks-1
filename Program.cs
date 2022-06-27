edd899eae0de865d34ed62814b4df86e2786370a
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-06-27 14:53:40 +0200

    snack 5

--- Program.cs
+++ Program.cs
@@ -1,25 +1,40 @@
-﻿//Snack 4
-int somma = 0;
-int media;
-int n = 0;
-for (int i = 2; i <= 10; i++)
-{
-    somma += i;
-    n++;
-}
-Console.WriteLine(somma);
-Console.WriteLine(n);
-media = somma / n;
-Console.WriteLine(media);
-
-
-return;
-//Snack 3
-int sum = 0;
-for (int i = 0; i < 10; i++)
+﻿//Snack 5
 {
     Console.Write("Inserisci un numero ");
     int newNum = Int32.Parse(Console.ReadLine());
-    sum += newNum;
+    if (newNum % 2 == 0)
+    {
+        Console.WriteLine(newNum);
+    } else
+    {
+        Console.WriteLine(newNum+1);
+    }
+}
+
+//Snack 4
+{
+    int sum = 0;
+    int media;
+    int n = 0;
+    for (int i = 2; i <= 10; i++)
+    {
+        sum += i;
+        n++;
+    }
+    Console.WriteLine(sum);
+    Console.WriteLine(n);
+    media = sum / n;
+    Console.WriteLine(media);
 };
-Console.WriteLine(sum);
+
+//Snack 3
+{
+    int sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        Console.Write("Inserisci un numero ");
+        int newNum = Int32.Parse(Console.ReadLine());
+        sum += newNum;
+    };
+    Console.WriteLine(sum);
+}
